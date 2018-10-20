44ad61db80d0399b11309e86ebe85536b1ccf2cf
Jacob <gordoj7@mcmaster.ca>
2018-10-20 17:08:41 -0400

    Added prebuilt phrases, and removed unneeded code from the text to speech

--- React Native Code/MEC_2k18/screens/LinksScreen.js
+++ React Native Code/MEC_2k18/screens/LinksScreen.js
@@ -1,27 +1,141 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { Text, Button, StyleSheet, View } from 'react-native';
+import { Constants, Speech } from 'expo';
+import Touchable from 'react-native-platform-touchable'; // 1.1.1
+import {AppRegistry, TextInput} from 'react-native';
+
+const PRESETS = [
+  {text: 'Yes' },
+  {text: 'No' },
+  {text: 'Hello' },
+  {text: 'How are you today?' },
+  {text: 'Oh yea' },
+];
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Preset',
+  };
+
+  state = {
+    selectedPreset: PRESETS[0],
+    text: "Please Enter Your Text",
+    inProgress: false,
+    pitch: 1,
+    rate: 0.75,
+    language: 'en'
   };
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerText}>Select a phrase</Text>
+        </View>
+
+        <View style={styles.examplesContainer}>
+          {PRESETS.map(this._renderPreset)}
+        </View>
+      </View>
     );
   }
+
+  _speak = (preset) => {
+    const start = () => {
+      this.setState({ inProgress: true });
+    };
+    const complete = () => {
+      this.state.inProgress && this.setState({ inProgress: false });
+    };
+
+    Speech.speak(preset.text, {
+      language: this.state.language,
+      pitch: this.state.pitch,
+      rate: this.state.rate,
+      onStart: start,
+      onDone: complete,
+      onStopped: complete,
+      onError: complete,
+    });
+  };
+
+  _stop = () => {
+    Speech.stop();
+  };
+
+
+  _renderPreset= (preset, i) => {
+    let { selectedPreset} = this.state;
+    let isSelected = selectedPreset === preset;
+
+    return (
+      <Touchable
+        key={i}
+        hitSlop={{ top: 10, bottom: 10, left: 20, right: 20 }}
+        onPress={() => this._selectPreset(preset)}>
+        <Text
+          style={[
+            styles.presetText,
+            isSelected && styles.selectedPresetText,
+          ]}>
+          {preset.text}
+        </Text>
+      </Touchable>
+    );
+  };
+
+  _selectPreset = preset => {
+    this.setState({ selectedPreset: preset });
+    this._speak(preset);
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 35,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#eee',
+    marginTop: 0,
+    marginBottom: 15,
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 5,
+  },
+  headerContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+    marginHorizontal: 20,
+    marginBottom: 0,
+    marginTop: 20,
+  },
+  presetText: {
+    fontSize: 15,
+    color: '#ccc',
+    marginVertical: 10,
+  },
+  presetContainer: {
     paddingTop: 15,
-    backgroundColor: '#fff',
+    paddingBottom: 10,
+    paddingHorizontal: 20,
+  },
+  selectedPresetText: {
+    color: 'black',
+  },
+  controlText: {
+    fontSize: 16,
+    fontWeight: '500',
+    marginTop: 5,
+    textAlign: 'center',
+  },
+  controlRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
   },
 });
